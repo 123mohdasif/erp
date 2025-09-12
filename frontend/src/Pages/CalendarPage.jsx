10c6ba5033781fa41fb57680b12201b8a13402f6
kushAug11 <kushagra1108.ss@gmail.com>
2025-09-12 23:32:12 +0530

    test

--- frontend/src/Pages/CalendarPage.jsx
+++ frontend/src/Pages/CalendarPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CalendarPage = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Calendar Page</h1>
+      <h1 className="text-2xl font-bold">Calendar Page////</h1>
       <p className="mt-2 text-gray-600">This is the calendar page content.</p>
     </div>
   );
